6a8f9fb8fdf929a3be18a5d5b7bbe68c2ecab3bc
devs.hashtag <devs.hashtag@gmail.com>
2022-10-21 23:27:32 +0330

    food random position added

--- assets/js/snakeGame/food.js
+++ assets/js/snakeGame/food.js
@@ -6,12 +6,39 @@ function Food(board, startPos = { left: 0, top: 0 }, type = 'snake-food') {
   this.type = type;
 
   // functions
+  // add food to board
   this.add = function (position) {
     this.block = this.board.block.add(this.type, position);
   };
 
-  this.move = function (block, position) {
-    this.board.block.move(block, position);
+  // move food inside board
+  this.move = function (position) {
+    this.board.block.move(this.block, position);
+  };
+
+  // move food to random position
+  this.random = function (boardBlocks) {
+    // all blocks positions
+    const blockPositions = boardBlocks.map((block) => this.board.position.block(block));
+    const blockSize = this.board.block.size;
+
+    let freePositions = [];
+
+    // find board free positions
+    for (let y = 0; y < this.board.height; y += blockSize) {
+      for (let x = 0; x < this.board.width; x += blockSize) {
+        if (!blockPositions.some((position) => position.left == x && position.top == y)) {
+          freePositions.push({ left: x, top: y });
+        } else {
+          console.log('removed block: ', { left: x, top: y });
+        }
+      }
+    }
+
+    // randomly choose a position
+    const position = freePositions[Math.floor(Math.random() * freePositions.length)];
+
+    this.move(position);
   };
 
   // food init
