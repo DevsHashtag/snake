2fd6779b1463bf62931e6f244367901cb3557c5f
devs.hashtag <devs.hashtag@gmail.com>
2022-10-23 21:48:39 +0330

    remove block added
    modalMessage added
    board blocks saved with classes

--- assets/js/snakeGame/board.js
+++ assets/js/snakeGame/board.js
@@ -1,4 +1,4 @@
-import { boardElement, GRID_COLUMNS, GRID_ROWS, GRID_BLOCK } from './settings.js';
+import { boardElement, GRID_COLUMNS, GRID_ROWS, GRID_BLOCK, BLOCK_CLASS } from './settings.js';
 
 import { unit } from './utils/unit.js';
 
@@ -11,7 +11,7 @@ function Board() {
   this.width = GRID_COLUMNS * GRID_BLOCK;
   this.height = GRID_ROWS * GRID_BLOCK;
 
-  this.blocks = [];
+  this.blocks = {};
 
   this.block = {
     board: this,
@@ -37,21 +37,45 @@ function Board() {
       }
 
       this.board.element.appendChild(block);
-      this.board.blocks.push(block);
+
+      if (!this.board.blocks[className[0]]) {
+        this.board.blocks[className[0]] = [block];
+      } else {
+        this.board.blocks[className[0]].push(block);
+      }
 
       return block;
+    },
+
+    remove: function (rmBlock) {
+      const blockClass = rmBlock.classList[0];
+      const boardBlocks = this.board.blocks[blockClass];
+
+      this.board.blocks[blockClass] = boardBlocks.filter((block) => !block.isSameNode(rmBlock));
+
+      if (!this.board.blocks[blockClass].length) delete this.board.blocks[blockClass];
+
+      this.board.element.removeChild(rmBlock);
+
+      return true;
     },
 
     move: function (block, position) {
       if (position.center) position = this.board.centerPosition();
       if (position.random) position = this.board.randomPosition();
 
-      if (!this.board.onBoard(position)) return false;
+      if (!position || !this.board.onBoard(position)) return false;
 
       block.style.left = unit.px(position.x);
       block.style.top = unit.px(position.y);
 
       return true;
+    },
+
+    modalMessage: function (msg, type) {
+      let msgElement = this.add(type, null, false);
+
+      msgElement.innerText = msg;
     },
   };
 
@@ -61,6 +85,8 @@ function Board() {
   };
 
   this.onBoard = function (pos) {
+    if (!pos) return false;
+
     let isOutBoard = [
       pos.y < 0, // out from up
       pos.y >= this.height, // out from down
@@ -74,6 +100,8 @@ function Board() {
   this.randomPosition = function () {
     const freePositions = this.freePositions();
 
+    if (!freePositions) return false;
+
     return freePositions[Math.floor(Math.random() * freePositions.length)];
   };
 
@@ -85,11 +113,13 @@ function Board() {
   };
 
   this.freePositions = function () {
+    const blocks = this.blocks[BLOCK_CLASS.snake.body];
+
     // return if no space is free
-    if (this.blocks.length >= this.columns * this.rows) return;
+    if (blocks.length >= this.columns * this.rows) return false;
 
     // blocks position
-    const blocksPosition = this.blocks.map((block) => unit.block(block));
+    const blocksPosition = blocks.map((block) => unit.block(block));
     const blockSize = this.block.size;
 
     let freePositions = [];
