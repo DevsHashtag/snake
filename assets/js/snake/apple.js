36b74eeed90a44186902f005686199d039b7c208
devs.hashtag <devs.hashtag@gmail.com>
2022-11-01 15:17:35 +0330

    apple.element renamed to apple.block
    apple.add removed
    apple.getPosition added

--- assets/js/snake/apple.js
+++ assets/js/snake/apple.js
@@ -4,33 +4,33 @@ import { isEqual } from './utils/position.js';
 import { board } from './app.js';
 
 function Apple() {
-  this.class = CLASS_NAMES.apple;
+  this.className = CLASS_NAMES.apple;
   this.initPosition = FOOD_POSITION;
-
-  this.add = function (position) {
-    this.element = board.addBlock(this.class, position);
-  };
 
   this.random = function () {
     this.animation();
 
-    board.moveBlock(this.element, { random: true });
+    board.moveBlock(this.block, { random: true });
   };
 
   this.onApple = function (position) {
-    const applePosition = board.blockPosition(this.element);
+    const applePosition = this.getPosition();
 
     return isEqual(applePosition, position);
   };
 
-  this.animation = function () {
-    this.element.style.backgroundColor = 'var(--bg-apple-moving)';
+  this.getPosition = function () {
+    return board.blockPosition(this.block);
+  };
 
-    setTimeout(() => this.element.style.removeProperty('background-color'), 300);
+  this.animation = function () {
+    this.block.style.backgroundColor = 'var(--bg-apple-moving)';
+
+    setTimeout(() => this.block.style.removeProperty('background-color'), 300);
   };
 
   this.init = function () {
-    this.add(this.initPosition);
+    this.block = board.addBlock(this.className, this.initPosition);
   };
 }
 
