c11bcd355795f78c2e321481ac382e023fac88ff
devs.hashtag <devs.hashtag@gmail.com>
2023-09-06 17:12:37 +0330

    snake/apple added

---
+++ assets/js/snake/apple.js
@@ -0,0 +1,35 @@
+import { board } from './game.js';
+
+function Apple() {
+  this.class = 'apple';
+
+  this.init = function () {
+    this.block = board.addBlock(this.class, { random: true });
+  };
+
+  this.random = function () {
+    this.animation();
+
+    board.setBlockPosition(this.block, { random: true });
+  };
+
+  this.animation = function (duration = 500) {
+    this.block.style.backgroundColor = 'var(--bg-apple-moving)';
+
+    setTimeout(() => {
+      this.block.style.removeProperty('background-color');
+    }, duration);
+  };
+
+  this.getPosition = function () {
+    return board.getBlockPosition(this.block);
+  };
+
+  this.isOnApple = function (position) {
+    const applePosition = this.getPosition();
+
+    return applePosition.x == position.x && applePosition.y == position.y;
+  };
+}
+
+export default Apple;
