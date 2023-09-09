89468185087fddedd680e7ef274a47d79990b0fd
devs.hashtag <devs.hashtag@gmail.com>
2023-09-10 01:16:25 +0330

    apple converted to new class syntax
    transition handeled by events

--- assets/js/snake/apple.js
+++ assets/js/snake/apple.js
@@ -1,35 +1,36 @@
 import { board } from './game.js';
 
-function Apple() {
-  this.class = 'apple';
-
-  this.init = function () {
+class Apple {
+  constructor() {
+    this.class = 'apple';
     this.block = board.addBlock(this.class, { random: true });
-  };
 
-  this.random = function () {
+    this.block.addEventListener('transitionend', () => {
+      if (this.block.style.backgroundColor) {
+        this.block.style.removeProperty('background-color');
+      }
+    });
+  }
+
+  random() {
     this.animation();
 
     board.setBlockPosition(this.block, { random: true });
-  };
+  }
 
-  this.animation = function (duration = 200) {
+  animation() {
     this.block.style.backgroundColor = 'var(--bg-apple-moving)';
+  }
 
-    setTimeout(() => {
-      this.block.style.removeProperty('background-color');
-    }, duration);
-  };
-
-  this.getPosition = function () {
+  getPosition() {
     return board.getBlockPosition(this.block);
-  };
+  }
 
-  this.isOnApple = function (position) {
+  isOnApple(position) {
     const applePosition = this.getPosition();
 
     return applePosition.x == position.x && applePosition.y == position.y;
-  };
+  }
 }
 
 export default Apple;
